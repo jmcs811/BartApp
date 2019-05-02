8effb370dde16edfb84fc7658d214d3c0857dfe8
jmcs811 <justincaseyy69@gmail.com>
2019-05-02 19:20:58 -0400

    Not much changed. Trying to figure out which way to do networking. retrofit or okhttp3.

--- app/src/main/java/com/jcaseydev/bart/ClosestStationFragment.java
+++ app/src/main/java/com/jcaseydev/bart/ClosestStationFragment.java
@@ -2,21 +2,21 @@ package com.jcaseydev.bart;
 
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ClosestStationFragment extends Fragment {
-
+    private TextView originStation;
+    private TextView destinationStation;
 
     public ClosestStationFragment() {
         // Required empty public constructor
@@ -25,9 +25,7 @@ public class ClosestStationFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
 
-        }
     }
 
 
@@ -35,6 +33,11 @@ public class ClosestStationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_closest_station, container, false);
+        View v = inflater.inflate(R.layout.fragment_closest_station, container, false);
+
+        originStation = v.findViewById(R.id.origin_textview);
+        destinationStation = v.findViewById(R.id.destination_textview);
+
+        return v;
     }
 }
