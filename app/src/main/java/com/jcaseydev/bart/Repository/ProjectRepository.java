3a38efc6ab5871a72873b8307984b2bed55a1d3f
jmcs811 <justincaseyy69@gmail.com>
2019-08-10 11:39:43 -0400

    Added ViewModel and Repository architecture to the train arrivals tab.

--- app/src/main/java/com/jcaseydev/bart/Repository/ProjectRepository.java
+++ app/src/main/java/com/jcaseydev/bart/Repository/ProjectRepository.java
@@ -2,6 +2,7 @@ package com.jcaseydev.bart.Repository;
 
 import androidx.lifecycle.MutableLiveData;
 import com.jcaseydev.bart.ApiInterface;
+import com.jcaseydev.bart.Model2.Arrivals.TrainArrival;
 import com.jcaseydev.bart.Model2.Stations.StationList;
 import com.jcaseydev.bart.RetrofitClient;
 import retrofit2.Call;
@@ -36,4 +37,25 @@ public class ProjectRepository {
     return stationList;
   }
 
+
+  public MutableLiveData<TrainArrival> getTrainArrivals() {
+    final MutableLiveData<TrainArrival> trainArrivals = new MutableLiveData<>();
+    apiInterface = RetrofitClient.getClient().create(ApiInterface.class);
+    Call<TrainArrival> call = apiInterface.getData();
+    call.enqueue(new Callback<TrainArrival>() {
+      @Override
+      public void onResponse(Call<TrainArrival> call, Response<TrainArrival> response) {
+        if (response.isSuccessful()) {
+          trainArrivals.setValue(response.body());
+        }
+      }
+
+      @Override
+      public void onFailure(Call<TrainArrival> call, Throwable t) {
+
+      }
+    });
+
+    return trainArrivals;
+  }
 }
--- app/src/main/java/com/jcaseydev/bart/StationListFragment.java
+++ app/src/main/java/com/jcaseydev/bart/StationListFragment.java
@@ -25,7 +25,6 @@ import java.util.List;
  */
 public class StationListFragment extends Fragment {
 
-  private StationList testList;
   private RecyclerView recyclerView;
   private StationListAdapter adapter;
   private StationListViewModel viewModel;
